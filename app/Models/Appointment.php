829044e063bbb296a762e08dcff0503d572d0c62
Jp12game <jos.pol.h@gmail.com>
2026-03-22 22:23:59 -0600

    Update Appointment.php

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+     protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'fecha_hora_inicio',
+        'fecha_hora_fin',
+        'status',
+    ];
+
+    // Relaciones
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
 }
